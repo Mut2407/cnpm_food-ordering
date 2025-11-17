2e6249bbda2fe22a439eff7708b60edcb2d38f86
Kỳ Vỹ <kyvy.2004@gmail.com>
2025-11-18 04:42:46 +0700

    update frontend ci

--- frontend/src/app/interceptor/auth-interceptor.interceptor.spec.ts
+++ frontend/src/app/interceptor/auth-interceptor.interceptor.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
 import { AuthInterceptor } from './auth-interceptor.interceptor';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthService } from '../service/auth.service';
 
 describe('AuthInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => AuthInterceptor(req, next));
+  
+  // Mock AuthService vì AuthInterceptor phụ thuộc vào nó
+  const mockAuthService = {
+    getToken: () => 'mock-token'
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Cần module này
+      providers: [
+        AuthInterceptor, // Cung cấp interceptor thật
+        { provide: AuthService, useValue: mockAuthService } // Cung cấp mock service
+      ]
+    });
   });
 
   it('should be created', () => {
+    // Inject class Interceptor
+    const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
     expect(interceptor).toBeTruthy();
   });
 });
